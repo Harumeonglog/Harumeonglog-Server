56ab4b5e7013b323ba688d773c42cc898b9c2584
Jeongmo Seo <wjdah1402@gmail.com>
2025-03-10 16:10:16 +0900

    :rocket: chore: 프로젝트 생성

---
+++ src/main/java/com/example/harumeonglog/HarumeonglogApplication.java
@@ -0,0 +1,13 @@
+package com.example.harumeonglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarumeonglogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarumeonglogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/harumeonglog/HarumeonglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harumeonglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarumeonglogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
